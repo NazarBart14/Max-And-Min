e9e6ad1f76cae727fb42f6ff11b38b549da8fb5a
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2022-07-24 13:07:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"");
+            int size = Convert.ToInt32(Console.ReadLine());
+          
+            Random random = new Random();
+            var arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(-100, 100);
+                Console.WriteLine($"{arr[i]}");
+              
+            }
+            Console.WriteLine($"Max = {arr.Max()}");
+            Console.WriteLine($"Min = {arr.Min()}");
+            Console.WriteLine($"Sum = {arr.Sum()}");
+           
+
+
+
+        }
+    }
+}
